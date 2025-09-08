c924f4ebf5b7c131e3f6e27f2a21a232f2a4a525
Enzo Quevedo <enzoquevedo85@gmail.com>
2025-09-08 13:55:13 -0300

    1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+Excursiones[
+    {
+        id: 1,
+        nombre: "Full Day Lagos Escondido y Fagnano", 
+        descripcion: "Los Lagos Escondido y Fagnano se encuentran en los profundos valles creados por los glaciares de la Cordillera de los Andes, rodeados de mágicos bosques. Esta excursión en Ushuaia nos invita a descubrirlos y explorarlos. El guía recogerá a los pasajeros por sus alojamientos rumbo a la primera parada: el mirador del Valle Carabajal, un punto único para tomar increíbles fotografías. Luego, ingresaremos en la zona de centros invernales, visitando uno de ellos. Continuando sobre la ruta Nacional N°3, cruzaremos la Cordillera de los Andes fueguinos, a una altura de 430 metros sobre el nivel del mar, observando el Lago Escondido desde elmirador del Paso Garibaldi, famoso en el mundo. Durante la excursión nos cruzaremos con las típicas Castoreras de Ushuaia, también conocidas como diques o madrigueras. Esta especie es una de las más famosas de toda la región austral. Luego llegaremos al Lago Fagnano, uno de los más grandes de la Argentina, cuyo nombre en idioma nativo es “Kami”. Junto a él, se encuentra la histórica estancia “La Carmen”, donde disfrutaremos de un café y una caminata. Como cierre de este mágico paseo almorzaremos en uno de los centros invernales los típicos platos de Ushuaia: cordero fueguino o trucha a la canoa(almuerzo no incluido, debe traerlo usted).",
+        ubicacion: "?????",
+        duracion: "6 horas",
+        precio: 130000, 
+        dificultad: "Facil",
+        incluye: ["Traslado", "Guía", "Entrada al parque"], 
+        imagen: "perito-moreno.jpg",
+        disponible: true
+
+    }
+]
